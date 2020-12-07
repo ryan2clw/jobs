2616c58d38eea8495118ca4fd6c8d779cb9dfc2d
Ryan Dines <ryan.dines@gamil.com>
2020-12-08 06:36:23 +0800

    Changed static dir.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,4 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/ryan2clw/jobs/static/"
+STATIC_ROOT = "/home/ryan2clw/react-jobs/build/static"
