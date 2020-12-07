5ebe2727f8247d8dfcb5f61ac5d975f275c26336
Ryan Dines <ryan.dines@gamil.com>
2020-12-08 01:12:13 +0800

    Added gunicorn and ALLOWEDD_HOSTS.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+#from .proxy_middleware import MultipleProxyMiddleware
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'xiv!m$v_g2me8dhw8lc0-((jkp^eknama^o!na$!1w^43d^fr('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['exp.host','localhost:3000']
+ALLOWED_HOSTS = ['api.seniordevops.com']
 
 
 # Application definition
@@ -127,3 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/home/ryan2clw/jobs/static/"
